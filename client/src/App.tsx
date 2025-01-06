3812714ed1c0afce0eb40262f2048ccc8c807c5d
Omri Rajuan <omri@webiks.com>
2025-01-06 02:49:59 +0200

    feat: integrate HomePage into App with design enhancements

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
+import { Box } from "@mui/material";
 import "./App.css";
-import SideBar from "./components/SideBar";
+import SideBar from "./layout/sideBar/SideBar";
+import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
-    <>
+    <Box sx={{ display: "flex" }}>
       <SideBar />
-    </>
+
+      <Box sx={{ flexGrow: 1 }}>
+        <HomePage />
+      </Box>
+    </Box>
   );
 }
 
