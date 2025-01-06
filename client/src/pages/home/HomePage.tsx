451504cc42cdcd3bc6fda8931c9f4ecf5eaa4f74
Omri Rajuan <omri@webiks.com>
2025-01-06 09:39:27 +0200

    feat(homepage): improve responsiveness

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,29 +1,40 @@
-import React from "react";
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import InfroCard from "./InfroCard";
 import VideoCard from "./VideoCard";
 
 const HomePage = () => {
   return (
-    <Box
+    <Container
       sx={{
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         gap: 4,
         marginTop: 4,
+        paddingX: 2,
+        width: "100%",
       }}
     >
-      <VideoCard
-        src="https://s3-alpha-sig.figma.com/img/66e3/3dec/1d50ec7c56cfea569c3daeda06734ffa?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EqVypQMn0G8V~Dww6RVNcT5oyP8dUtIeC~WF6Kfqr~0gZSlmFh0ESLRM2W6aGmJsimAHPR~JXX1jGN-uK2Orkizjb1VbpEPVV7XFENSlH5sjomJlRU~CeyrUPAY0WlEkCBi-qjjNwpynG3Rl5nVlv1GzSi9tQAIFoeqaljZlzNLn41iDdOgB5ORenRIA6rpdRh4H5BbTD8XoVnHnEQub64lIA-sCtyxXXRvbB2XbPNvXIhT2BncIoIXFFDe1WNH7JfoeNsdZK~UhUb3-g0hqZqu3INz3pGFjZH2bdS88nQQCrEA7B~84Ca1jkaDrbo~QL08WrCer2Bz69MFwg76wmA__"
-        alt="placeholder"
-      />
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: "798px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <VideoCard
+          src="https://s3-alpha-sig.figma.com/img/66e3/3dec/1d50ec7c56cfea569c3daeda06734ffa?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EqVypQMn0G8V~Dww6RVNcT5oyP8dUtIeC~WF6Kfqr~0gZSlmFh0ESLRM2W6aGmJsimAHPR~JXX1jGN-uK2Orkizjb1VbpEPVV7XFENSlH5sjomJlRU~CeyrUPAY0WlEkCBi-qjjNwpynG3Rl5nVlv1GzSi9tQAIFoeqaljZlzNLn41iDdOgB5ORenRIA6rpdRh4H5BbTD8XoVnHnEQub64lIA-sCtyxXXRvbB2XbPNvXIhT2BncIoIXFFDe1WNH7JfoeNsdZK~UhUb3-g0hqZqu3INz3pGFjZH2bdS88nQQCrEA7B~84Ca1jkaDrbo~QL08WrCer2Bz69MFwg76wmA__"
+          alt="placeholder"
+        />
+      </Box>
       <Box
         sx={{
           display: "flex",
           gap: 3,
           width: "100%",
           maxWidth: "798px",
+          flexDirection: { xs: "column", md: "row" },
         }}
       >
         <InfroCard
@@ -35,7 +46,7 @@ const HomePage = () => {
           content="A dialog is a type of modal window that appears in front of app content to provide critical information, or prompt for a decision to be made."
         />
       </Box>
-    </Box>
+    </Container>
   );
 };
 
