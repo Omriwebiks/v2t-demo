a44065f48ab6a55af51cf96f57845c6a84e809b8
Omri Rajuan <omri@webiks.com>
2025-01-07 13:29:13 +0200

    fix: update card position after product review

--- client/src/pages/library/VideoImg.tsx
+++ client/src/pages/library/VideoImg.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Box, Card, Typography } from "@mui/material";
 
 interface VideoImgProps {
   title: string;
@@ -6,25 +6,23 @@ interface VideoImgProps {
 }
 
 const VideoImg = ({ title, project }: VideoImgProps) => (
-  <Card sx={{ boxShadow: "none" }}>
-    <CardContent>
-      <Box
-        component="img"
-        src="https://s3-alpha-sig.figma.com/img/66e3/3dec/1d50ec7c56cfea569c3daeda06734ffa?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EqVypQMn0G8V~Dww6RVNcT5oyP8dUtIeC~WF6Kfqr~0gZSlmFh0ESLRM2W6aGmJsimAHPR~JXX1jGN-uK2Orkizjb1VbpEPVV7XFENSlH5sjomJlRU~CeyrUPAY0WlEkCBi-qjjNwpynG3Rl5nVlv1GzSi9tQAIFoeqaljZlzNLn41iDdOgB5ORenRIA6rpdRh4H5BbTD8XoVnHnEQub64lIA-sCtyxXXRvbB2XbPNvXIhT2BncIoIXFFDe1WNH7JfoeNsdZK~UhUb3-g0hqZqu3INz3pGFjZH2bdS88nQQCrEA7B~84Ca1jkaDrbo~QL08WrCer2Bz69MFwg76wmA__"
-        alt="placeholder"
-        sx={{
-          width: "80%",
-          maxWidth: "100%",
-          borderRadius: "8%",
-        }}
-      />
-      <Typography variant="h6" component="div">
-        {title}
-      </Typography>
-      <Typography variant="body2" color="text.secondary">
-        Project: {project}
-      </Typography>
-    </CardContent>
+  <Card sx={{ boxShadow: "none", marginTop: "20px" }}>
+    <Box
+      component="img"
+      src="https://s3-alpha-sig.figma.com/img/66e3/3dec/1d50ec7c56cfea569c3daeda06734ffa?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EqVypQMn0G8V~Dww6RVNcT5oyP8dUtIeC~WF6Kfqr~0gZSlmFh0ESLRM2W6aGmJsimAHPR~JXX1jGN-uK2Orkizjb1VbpEPVV7XFENSlH5sjomJlRU~CeyrUPAY0WlEkCBi-qjjNwpynG3Rl5nVlv1GzSi9tQAIFoeqaljZlzNLn41iDdOgB5ORenRIA6rpdRh4H5BbTD8XoVnHnEQub64lIA-sCtyxXXRvbB2XbPNvXIhT2BncIoIXFFDe1WNH7JfoeNsdZK~UhUb3-g0hqZqu3INz3pGFjZH2bdS88nQQCrEA7B~84Ca1jkaDrbo~QL08WrCer2Bz69MFwg76wmA__"
+      alt="placeholder"
+      sx={{
+        width: "80%",
+        maxWidth: "100%",
+        borderRadius: "8%",
+      }}
+    />
+    <Typography variant="h6" component="div">
+      {title}
+    </Typography>
+    <Typography variant="body2" color="text.secondary">
+      Project: {project}
+    </Typography>
   </Card>
 );
 
